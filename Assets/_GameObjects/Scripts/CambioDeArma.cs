aff5f5be46552b5b15f66a75a379f409e88b7738
devlex13 <46582177+devlex13@users.noreply.github.com>
2019-02-20 02:51:19 +0100

    v2

--- Assets/_GameObjects/Scripts/CambioDeArma.cs
+++ Assets/_GameObjects/Scripts/CambioDeArma.cs
@@ -34,6 +34,42 @@ public class CambioDeArma : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            anim.Play("AnimacionCambioArma");
+
+            negroRojoCadera.SetActive(true);
+            negroAzulCadera.SetActive(false);
+            grisDoradoCadera.SetActive(false);
+
+            negroRojoApuntando.SetActive(true);
+            negroAzulApuntando.SetActive(false);
+            grisDoradoApuntando.SetActive(false);
+
+        }else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            anim.Play("AnimacionCambioArma");
+
+            negroRojoCadera.SetActive(false);
+            negroAzulCadera.SetActive(true);
+            grisDoradoCadera.SetActive(false);
+
+            negroRojoApuntando.SetActive(false);
+            negroAzulApuntando.SetActive(true);
+            grisDoradoApuntando.SetActive(false);
+        }else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            anim.Play("AnimacionCambioArma");
+
+            negroRojoCadera.SetActive(false);
+            negroAzulCadera.SetActive(false);
+            grisDoradoCadera.SetActive(true);
+
+            negroRojoApuntando.SetActive(false);
+            negroAzulApuntando.SetActive(false);
+            grisDoradoApuntando.SetActive(true);
+        }
+
         if (Input.GetButtonDown("CambiarArma"))
         {
             numeroArma++;
@@ -73,6 +109,7 @@ public class CambioDeArma : MonoBehaviour {
             }
             
         }
+
 
 	}
 }
